9502725282d88fe6805476685cc28003b43609ae
endorphin82 <endorphin82@gmail.com>
2020-06-03 19:18:32 +0300

    custom type gql hooks

--- gqlapi/models_prs/schema_types/Album.ts
+++ gqlapi/models_prs/schema_types/Album.ts
@@ -7,24 +7,5 @@ export const Album = objectType({
     t.model.name()
     t.model.year()
     t.model.artist()
-    //
-    // t.list.field('artists', {
-    //   type: 'Artist',    // @ts-ignore
-    //   resolve: async ({ id }, args, context) => {
-    //     // @ts-ignore
-    //     // return await context.prisma.album({ id }).artists()
-    //     return await context.prisma.album.findOne({ id }).ArtistAlbum({ where: { album_id: id } })
-    //   }
-    // })
-    // t.list.field('artists', {
-    //   resolve: ({ id }, args, context) => {
-    //     return context.prisma.artistAlbum.findMany({ where: { album_id: id } })
-    //   }
-    // })
-    // t.list.field('artists', {
-    //   resolve: async (root, args, context) => {
-    //     return await context.prisma.artistAlbum.findMany({ where: { album_id: root.id } })
-    //   }
-    // })
   }
 })
