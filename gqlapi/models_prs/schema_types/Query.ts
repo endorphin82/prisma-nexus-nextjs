5099f9932b0d2c1708dc67c53b44b86dd9af2525
endorphin82 <endorphin82@gmail.com>
2020-06-03 13:37:45 +0300

    custom query artistsByName

--- gqlapi/models_prs/schema_types/Query.ts
+++ gqlapi/models_prs/schema_types/Query.ts
@@ -1,12 +1,24 @@
-import { objectType } from '@nexus/schema'
+import { objectType, stringArg } from '@nexus/schema'
 
 export const Query = objectType({
   name: 'Query',
-  definition(t) {
+  definition: function(t) {
     t.crud.album()
     t.crud.albums()
     t.crud.artist()
     t.crud.artists()
+
+
+    t.field('artistsByName', {
+      type: 'Artist',
+      list: true,
+      args: {
+        name: stringArg({ required: true })
+      },
+      resolve: (_, { name }, ctx) =>
+        ctx.prisma.artist.findMany({ where: { name } })
+    })
+
     // t.list.field("artists", {
     //   type: "Artist",
     //   resolve: (_, args, ctx) =>
